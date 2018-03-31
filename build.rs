4dbac4b122c3dbeb2b4fe52e9c6bf67371e81557
Adrien Gruson <adrien.gruson@gmail.com>
2018-03-31 17:39:22 +0900

    change to rust target 1.25 for bindgen.

--- build.rs
+++ build.rs
@@ -11,18 +11,18 @@ fn main() {
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
-    let bindings = bindgen::Builder::default()
+    let builder = bindgen::Builder::default()
         .clang_args(["-x", "c++", "-std=c++11"].iter())
         .enable_cxx_namespaces()
         // Remove max align as it does not pass the test
         .blacklist_type("max_align_t")
-        // The input header we would like to generate
-        // bindings for.
-        .header("wrapper.h")
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+        .rust_target(bindgen::RustTarget::Stable_1_25)
+        .header("wrapper.h");
+
+    let commands = builder.command_line_flags();
+    println!("Commands from bindgen: {:?}", commands);
+
+    let bindings = builder.generate().expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = env::var("OUT_DIR").unwrap();
--- wrapper.h
+++ wrapper.h
@@ -1,2 +1,4 @@
+// bindgen-flags: --rust-target 1.25 -- -std=c++11
+
 #include <embree2/rtcore.h>
 #include <embree2/rtcore_ray.h>
